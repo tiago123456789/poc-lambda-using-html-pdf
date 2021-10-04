a8b1303cdcca8b97a91df6103916505708b19ce6
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-10-04 12:05:34 -0300

    Changes

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ process.env["FONTCONFIG_PATH"] = "/var/task/fonts"
 var options = { format: 'Letter', phantomPath: './phantom_lambda/phantomjs_linux-x86_64' };
 
 module.exports.hello = function (event, context, callback) {
-  var htmlString = "<body>Hi, my friend!!!</body>"
+  var htmlString = "<body>Hi, my friend!!!</body>" 
 
   pdf.create(htmlString, options).toBuffer(function (err, buffer) {
     if (err) {
