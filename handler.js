b558b65b67e1175efcc78deef7cee1a4c9b5e52d
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-10-04 12:14:03 -0300

    Changes

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@ var pdf = require('html-pdf');
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 process.env["FONTCONFIG_PATH"] = "/var/task/fonts"
 
-var options = { format: 'Letter', phantomPath: './phantom_lambda/phantomjs_linux-x86_64' };
+var options = { format: 'Letter', phantomPath: './phantomjs_linux-x86_64' };
 
 module.exports.hello = function (event, context, callback) {
   var htmlString = "<body>Hi, my friend!!!</body>" 
